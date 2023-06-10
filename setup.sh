48dba6264ad329320668b7cb3959bdaaff40af73
kernel-dev <twitchisadeck@gmail.com>
2023-06-10 20:24:05 +0200

    update(setup): switch to AUDK and RELEASE versions of Pkgs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Move to EDK2 directory
-cd /Users/kernel/Documents/edk2-oofer/edk2/
+cd /Users/user/Documents/audk/
 
 # Source edksetup.sh to reconfigure config
 . edksetup.sh
@@ -8,13 +8,13 @@ make -C BaseTools
 
 # Compile project
 cd BootloaderPkg
-build -a X64 -t XCODE5 -b DEBUG -p OvmfPkg/OvmfPkgX64.dsc -D SOURCE_DEBUG_ENABLE=TRUE -D DEBUG_ON_SERIAL_PORT || exit 1
-build -a X64 -t XCODE5 -b DEBUG -p MdeModulePkg/MdeModulePkg.dsc || exit 1
+build -a X64 -t XCODE5 -b RELEASE -p OvmfPkg/OvmfPkgX64.dsc || exit 1
+build -a X64 -t XCODE5 -b RELEASE -p MdeModulePkg/MdeModulePkg.dsc || exit 1
 
 # Copy BIOS
-cp /Users/kernel/Documents/edk2-master/edk2/Build/OvmfX64/RELEASE_XCODE5/FV/OVMF.fd ../KernelOSPkg/bootloader/bios.bin
+cp /Users/user/Documents/audk/Build/OvmfX64/RELEASE_XCODE5/FV/OVMF.fd ../KernelOSPkg/bootloader/bios.bin
 
 # Copy EFI binary
 mkdir EFI 2> /dev/null
-cp /Users/kernel/Documents/edk2-oofer/edk2/Build/MdeModule/DEBUG_XCODE5/X64/BootloaderPkg/BootloaderPkg/OUTPUT/KernelOSBootloader.efi EFI/KernelOSBootloader.efi
-cp ./EFI/KernelOSBootloader.efi /Users/kernel/Documents/edk2-oofer/edk2/KernelOSPkg/hda-contents/EFI/BOOT/BOOTX64.efi
+cp /Users/user/Documents/audk/Build/MdeModule/RELEASE_XCODE5/X64/BootloaderPkg/BootloaderPkg/OUTPUT/KernelOSBootloader.efi EFI/KernelOSBootloader.efi
+cp ./EFI/KernelOSBootloader.efi /Users/user/Documents/audk/KernelOSPkg/hda-contents/EFI/BOOT/BOOTX64.efi
