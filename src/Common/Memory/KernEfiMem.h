ae00116af28a38df338b11e91e92a073924b2043
kernel-dev <twitchisadeck@gmail.com>
2023-06-11 09:10:29 +0200

    fix(MemMap): obtain system memory map in the `KernelLoader.c` portion of the codebase

--- src/Common/Memory/KernEfiMem.h
+++ src/Common/Memory/KernEfiMem.h
@@ -26,22 +26,4 @@ typedef struct {
     BOOLEAN                 Empty;
 } EFI_KERN_MEMORY_MAP;
 
-/**
-    Attempts to obtain the system memory map
-    right before calling the kernel's EP.
-
-    @param[in]  ImageHandle  The image handle.
-    @param[in]  SystemTable  A pointer to the system table.
-
-    @returns    A memory map contained in an
-                EFI_KERN_MEMORY_MAP struct.
-                The ".Empty" property will be
-                TRUE if something went wrong,
-                otherwise FALSE
- **/
-EFI_KERN_MEMORY_MAP
-EfiKernGetMemoryMap (
-    IN EFI_HANDLE           ImageHandle, 
-    IN EFI_SYSTEM_TABLE     *SystemTable);
-
 #endif /* KernEfiMem.h */
