69d02c1b1a98003676177ab5265204c52d19e41c
kernel-dev <twitchisadeck@gmail.com>
2023-07-31 19:26:32 +0200

    update: `setup.sh`

--- setup.sh
+++ setup.sh
@@ -19,18 +19,18 @@ make -C BaseTools
 
 cd $DIRPATH
 
-echo "Compiling BIOS and Bootloader..."
 # Compile project
+echo "Compiling BIOS and Bootloader..."
 cd $DIRPATH
 build -a X64 -t XCODE5 -b DEBUG -p OvmfPkg/OvmfPkgX64.dsc || exit 1
 build -a X64 -t XCODE5 -b DEBUG -p MdeModulePkg/MdeModulePkg.dsc || exit 1
 
-echo "Copying BIOS..."
 # Copy BIOS
+echo "Copying BIOS..."
 cp $PARPATH/Build/OvmfX64/DEBUG_XCODE5/FV/OVMF.fd ../KernelOSPkg/bootloader/bios.bin
 
-echo "Copying EFI application..."
 # Copy EFI binary
-mkdir EFI 2> /dev/null
+echo "Copying EFI application..."
+mkdir EFI 2>/dev/null
 cp $PARPATH/Build/MdeModule/DEBUG_XCODE5/X64/BootloaderPkg/BootloaderPkg/OUTPUT/KernelOSBootloader.efi EFI/KernelOSBootloader.efi
 cp ./EFI/KernelOSBootloader.efi $PARPATH/KernelOSPkg/hda-contents/EFI/BOOT/BOOTX64.efi
